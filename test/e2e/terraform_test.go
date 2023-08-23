55d724e1c9d43ac410a33525ea70237d3758d5e5
oribenartzy <oribenartzy@microsoft.com>
2023-08-23 10:24:30 +0300

    Add new plans to test

--- test/e2e/terraform_test.go
+++ test/e2e/terraform_test.go
@@ -22,6 +22,8 @@ func TestExamples_single_subscription(t *testing.T) {
 		"CosmosDbs",
 		"StorageAccounts",
 		"VirtualMachines",
+		"CloudPosture",
+		"Api",
 	}
 	test_helper.RunE2ETest(t, "../../", "examples/single_subscription", terraform.Options{
 		Upgrade: true,

e082b002e57bcf9eb0e2c55e445bc44835eda8a5
zjhe <hezijie@microsoft.com>
2023-10-25 07:55:18 +0800

    turn off the telemetry for example, we don't want collect telemetry from our own testing environment

--- examples/single_subscription/versions.tf
+++ examples/single_subscription/versions.tf
@@ -6,9 +6,17 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.7.0, < 4.0"
     }
+    modtm = {
+      source = "Azure/modtm"
+      version = ">= 0.1.7, < 1.0"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
+}
+
+provider "modtm" {
+  enabled = false
 }

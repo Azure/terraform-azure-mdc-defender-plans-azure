1cc95387e74aeff645404998b7899ed2fa9f468c
zjhe <hezijie@microsoft.com>
2023-10-24 15:54:02 +0800

    add telemetry resource

---
+++ module_telemetry.tf
@@ -0,0 +1,3 @@
+resource "modtm_telemetry" "this" {
+
+}
--- versions.tf
+++ versions.tf
@@ -6,5 +6,9 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.47, < 4.0"
     }
+    modtm = {
+      source = "Azure/modtm"
+      version = ">= 0.1.7, < 1.0"
+    }
   }
 }

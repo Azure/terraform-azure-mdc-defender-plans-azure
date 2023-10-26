011638b1aa33d2d09c5f5c1807fd2678835246a2
hezijie <lonegunmanb@hotmail.com>
2023-10-26 15:48:52 +0800

    add ignore_changes

--- module_telemetry.tf
+++ module_telemetry.tf
@@ -1,7 +1,10 @@
 resource "modtm_telemetry" "this" {
   tags = {
-
     avm_yor_name  = "this"
     avm_yor_trace = "b7642d6f-c1b3-45da-8837-9a64e8b7396e"
+  }
+
+  lifecycle {
+    ignore_changes = [nonce]
   }
 }

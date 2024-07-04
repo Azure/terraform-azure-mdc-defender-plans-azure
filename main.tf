3d14e6faa72dc5c434592baf7bf6e99802ad82fa
Eli Betito <elibetito@microsoft.com>
2024-07-04 15:21:26 +0300

    Merge all extensions inside azurerm_security_center_subscription_pricing.asc_plans, upgrade storage subplan and rename upgradeE2E alias

--- main.tf
+++ main.tf
@@ -1,20 +1,19 @@
 locals {
-  final_plans_list        = contains(var.mdc_plans_list, "CloudPosture") ? setsubtract(setunion(local.plans_without_databases), ["CloudPosture"]) : local.plans_without_databases
   plans_without_databases = contains(var.mdc_plans_list, "Databases") ? setsubtract(setunion(var.mdc_plans_list, var.mdc_databases_plans), ["Databases"]) : var.mdc_plans_list
 }
 
 data "azurerm_subscription" "current" {}
 
 resource "azurerm_security_center_subscription_pricing" "asc_plans" {
-  for_each = local.final_plans_list
+  for_each = local.plans_without_databases
 
   tier          = var.default_tier
   resource_type = each.value
-  # For "StorageAccounts" subplan is "PerStorageAccount". For other plans subplan is null.
-  subplan = lookup(var.subplans, each.key, each.key == "StorageAccounts" ? "PerStorageAccount" : var.default_subplan)
+  # Apply subplan only For "StorageAccounts". For other plans, subplan is null.
+  subplan = lookup(var.subplans, each.key, each.key == "StorageAccounts" ? "DefenderForStorageV2" : var.default_subplan)
 
   dynamic "extension" {
-    for_each = each.key == "VirtualMachines" ? [1] : []
+    for_each = each.key == "VirtualMachines" || each.key == "CloudPosture" ? [1] : []
     content {
       name = "AgentlessVmScanning"
       additional_extension_properties = {
@@ -23,15 +22,36 @@ resource "azurerm_security_center_subscription_pricing" "asc_plans" {
     }
   }
   dynamic "extension" {
-    for_each = each.key == "Containers" ? [1] : []
+    for_each = each.key == "Containers" || each.key == "CloudPosture" ? [1] : []
     content {
       name = "ContainerRegistriesVulnerabilityAssessments"
     }
   }
   dynamic "extension" {
-    for_each = each.key == "Containers" ? [1] : []
+    for_each = each.key == "Containers" || each.key == "CloudPosture" ? [1] : []
     content {
       name = "AgentlessDiscoveryForKubernetes"
+    }
+  }
+  dynamic "extension" {
+    for_each = each.key == "StorageAccounts" ? [1] : []
+    content {
+      name = "OnUploadMalwareScanning"
+      additional_extension_properties = {
+        CapGBPerMonthPerStorageAccount = "5000"
+      }
+    }
+  }
+  dynamic "extension" {
+    for_each = each.key == "CloudPosture" || each.key == "StorageAccounts" ? [1] : []
+    content {
+      name = "SensitiveDataDiscovery"
+    }
+  }
+  dynamic "extension" {
+    for_each = each.key == "CloudPosture" ? [1] : []
+    content {
+      name = "EntraPermissionsManagement"
     }
   }
 }

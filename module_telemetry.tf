fd9b7097c92e63ebaf95dadfd5132c0c0370ae81
Eli Betito <elibetito@microsoft.com>
2024-07-08 09:57:39 +0300

    revert modtm_telemetry ephemeral_number

--- module_telemetry.tf
+++ module_telemetry.tf
@@ -8,4 +8,9 @@ resource "modtm_telemetry" "this" {
     avm_git_org              = "Azure"
     avm_git_repo             = "terraform-azure-mdc-defender-plans-azure"
   }
+  ephemeral_number = 12264
+
+  lifecycle {
+    ignore_changes = [ephemeral_number]
+  }
 }

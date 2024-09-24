0c4f7de47f216f4962404092ddb4087bc6c33b6a
hezijie <lonegunmanb@hotmail.com>
2024-09-24 14:31:38 +0800

    fix e2e test

--- examples/single_subscription/main.tf
+++ examples/single_subscription/main.tf
@@ -1,5 +1,6 @@
 module "mdc_plans_enable" {
-  source         = "../.."
-  mdc_plans_list = var.mdc_plans_list
-  subplans       = var.subplans
+  source           = "../.."
+  mdc_plans_list   = var.mdc_plans_list
+  subplans         = var.subplans
+  enable_telemetry = var.enable_telemetry
 }

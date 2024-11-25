9c67ca0b89690f4eab451a995d92011f0986eef0
hezijie <lonegunmanb@hotmail.com>
2024-11-25 14:33:40 +0800

    avmfix

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 locals {
-  plans_without_databases = contains(var.mdc_plans_list, "Databases") ? setsubtract(setunion(var.mdc_plans_list, var.mdc_databases_plans), ["Databases"]) : var.mdc_plans_list
   plan_extenstions = {
-    AgentlessVmScanning =  tolist(["VirtualMachines", "CloudPosture", "Containers"])
+    AgentlessVmScanning                         = tolist(["VirtualMachines", "CloudPosture", "Containers"])
     ContainerRegistriesVulnerabilityAssessments = tolist(["Containers", "CloudPosture"])
-    AgentlessDiscoveryForKubernetes = tolist(["Containers", "CloudPosture"])
-    ContainerSensor = tolist(["Containers"])
-    OnUploadMalwareScanning = tolist(["StorageAccounts"])
-    SensitiveDataDiscovery = tolist(["CloudPosture", "StorageAccounts"])
-    EntraPermissionsManagement = tolist(["CloudPosture"])
+    AgentlessDiscoveryForKubernetes             = tolist(["Containers", "CloudPosture"])
+    ContainerSensor                             = tolist(["Containers"])
+    OnUploadMalwareScanning                     = tolist(["StorageAccounts"])
+    SensitiveDataDiscovery                      = tolist(["CloudPosture", "StorageAccounts"])
+    EntraPermissionsManagement                  = tolist(["CloudPosture"])
   }
+  plans_without_databases = contains(var.mdc_plans_list, "Databases") ? setsubtract(setunion(var.mdc_plans_list, var.mdc_databases_plans), ["Databases"]) : var.mdc_plans_list
 }
 
 data "azurerm_subscription" "current" {}

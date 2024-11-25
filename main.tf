1abcd40e1da0ea924617e430039385e36d99659b
lonegunmanb <lonegunmanb@hotmail.com>
2024-11-25 14:31:58 +0800

    add AgentlessVmScanning extension for Containers to ensure config idempotent

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
 locals {
   plans_without_databases = contains(var.mdc_plans_list, "Databases") ? setsubtract(setunion(var.mdc_plans_list, var.mdc_databases_plans), ["Databases"]) : var.mdc_plans_list
+  plan_extenstions = {
+    AgentlessVmScanning =  tolist(["VirtualMachines", "CloudPosture", "Containers"])
+    ContainerRegistriesVulnerabilityAssessments = tolist(["Containers", "CloudPosture"])
+    AgentlessDiscoveryForKubernetes = tolist(["Containers", "CloudPosture"])
+    ContainerSensor = tolist(["Containers"])
+    OnUploadMalwareScanning = tolist(["StorageAccounts"])
+    SensitiveDataDiscovery = tolist(["CloudPosture", "StorageAccounts"])
+    EntraPermissionsManagement = tolist(["CloudPosture"])
+  }
 }
 
 data "azurerm_subscription" "current" {}
@@ -13,7 +22,7 @@ resource "azurerm_security_center_subscription_pricing" "asc_plans" {
   subplan = lookup(var.subplans, each.key, each.key == "StorageAccounts" ? "DefenderForStorageV2" : var.default_subplan)
 
   dynamic "extension" {
-    for_each = each.key == "VirtualMachines" || each.key == "CloudPosture" ? [1] : []
+    for_each = try(contains(local.plan_extenstions["AgentlessVmScanning"], each.key), false) ? [1] : []
 
     content {
       name = "AgentlessVmScanning"
@@ -23,28 +32,28 @@ resource "azurerm_security_center_subscription_pricing" "asc_plans" {
     }
   }
   dynamic "extension" {
-    for_each = each.key == "Containers" || each.key == "CloudPosture" ? [1] : []
+    for_each = try(contains(local.plan_extenstions["ContainerRegistriesVulnerabilityAssessments"], each.key), false) ? [1] : []
 
     content {
       name = "ContainerRegistriesVulnerabilityAssessments"
     }
   }
   dynamic "extension" {
-    for_each = each.key == "Containers" || each.key == "CloudPosture" ? [1] : []
+    for_each = try(contains(local.plan_extenstions["AgentlessDiscoveryForKubernetes"], each.key), false) ? [1] : []
 
     content {
       name = "AgentlessDiscoveryForKubernetes"
     }
   }
   dynamic "extension" {
-    for_each = each.key == "Containers" ? [1] : []
+    for_each = try(contains(local.plan_extenstions["ContainerSensor"], each.key), false) ? [1] : []
 
     content {
       name = "ContainerSensor"
     }
   }
   dynamic "extension" {
-    for_each = each.key == "StorageAccounts" ? [1] : []
+    for_each = try(contains(local.plan_extenstions["OnUploadMalwareScanning"], each.key), false) ? [1] : []
 
     content {
       name = "OnUploadMalwareScanning"
@@ -54,14 +63,14 @@ resource "azurerm_security_center_subscription_pricing" "asc_plans" {
     }
   }
   dynamic "extension" {
-    for_each = each.key == "CloudPosture" || each.key == "StorageAccounts" ? [1] : []
+    for_each = try(contains(local.plan_extenstions["SensitiveDataDiscovery"], each.key), false) ? [1] : []
 
     content {
       name = "SensitiveDataDiscovery"
     }
   }
   dynamic "extension" {
-    for_each = each.key == "CloudPosture" ? [1] : []
+    for_each = try(contains(local.plan_extenstions["EntraPermissionsManagement"], each.key), false) ? [1] : []
 
     content {
       name = "EntraPermissionsManagement"
